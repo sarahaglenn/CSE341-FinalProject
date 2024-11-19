c30defb3550ccaf257e3046107070b75e9910e2f
Sarah Glenn <sarahadaline@gmail.com>
2024-11-19 19:28:12 +0100

    created swagger.js file Current host is development url. Also created routes/swagger.js  and added to server.js

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./autoSwagger.json');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()).use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
