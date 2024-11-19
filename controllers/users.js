00518e40355825c8bcc02f79027ae13e5a6a0cb1
Sarah Glenn <sarahadaline@gmail.com>
2024-11-19 09:40:28 +0100

    controllers/users GET functions filled in except for login and logout

---
+++ controllers/users.js
@@ -0,0 +1,59 @@
+const User = require('../models/user-model');
+const { ObjectId } = require('mongodb');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving users', detail: error.message });
+  }
+};
+
+const getUserById = async (req, res) => {
+  if (!ObjectId.isValid(req.params.userId)) {
+    return res.status(400).json({ error: 'Must use a valid user id to find a user.' });
+  }
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId);
+    if (user) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: 'No user exists with that id' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving user.', detail: error.message });
+  }
+};
+
+const getUserByType = async (req, res) => {
+  if (!req.params.userType.toLower() == 'patron' || !req.params.userType.toLower() == 'staff') {
+    return res.status(400).json({ error: 'Must use a valid user type to find a user.' });
+  }
+  const userType = req.params.userType.toLower();
+  try {
+    const users = await User.find({ UserType: userType });
+    if (users) {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(users);
+    } else {
+      res.status(404).json({ error: 'No users exist with that user type.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error retrieving users.', detail: error.message });
+  }
+};
+
+const userLogin = async (req, res) => {};
+const userLogout = async (req, res) => {};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  getUserByType,
+  userLogin,
+  userLogout
+};
